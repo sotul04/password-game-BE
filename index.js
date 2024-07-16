16bfdc8ae2031706cf0b2b725c74097690ca1502
sotul04 <109208105+sotul04@users.noreply.github.com>
2024-07-16 18:20:03 +0700

    fix: vercel scripts updated

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const defaultSchema = {
     }
 }
 //port used
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const express = require('express');
 const mongoose = require('mongoose');
